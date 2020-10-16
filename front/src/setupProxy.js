a78f8b6fbc94fc36b418c1cd324ab6c2c7bf381f
lss0815 <js815002@gmail.com>
2020-10-16 22:49:58 +0000

    set up proxy

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app){
-	app.use(createProxyMiddleware( {'/back' ,target: 'http://34.64.182.81:8000', changeOrigin: true}));	
+	app.use(createProxyMiddleware( '/back' ,{target: 'http://34.64.182.81:8000', changeOrigin: true}));	
 };
