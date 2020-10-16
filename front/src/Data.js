50b382083412afe65810ae4a37af48f17a3fce27
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-17 05:23:09 +0900

    no message

--- front/src/Data.js
+++ front/src/Data.js
@@ -4,7 +4,7 @@ class Data extends Component {
     state = {
         data : [],
     }
-    fetch("/data")
+    fetch('/data')
         .then(res => res.json())
         .then(data => {
             this.setState({
