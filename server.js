a986985f0600d0d12d466e1f260d56efee035b51
lss0815 <js815002@gmail.com>
2020-10-16 23:02:31 +0000

    cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
- 
+const cors = require('cors');
+
+app.use(cors());
+
 app.get('/data',(req,res)=>{
     const data = {
         lastname : "dl",
