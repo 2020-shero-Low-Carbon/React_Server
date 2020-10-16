ed6fa46a19b23b8ec921ac63be8c8df62d7c5b9f
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-17 08:49:31 +0900

    no message

--- front/src/Data.js
+++ front/src/Data.js
@@ -5,11 +5,10 @@ class Data extends Component {
         posts : [],
     }
     componentWillMount() {
-        console.log("hi console")
         fetch('http://34.64.182.81:8000/data')
             .then(response => response.json())
-            .then(dataw => this.setState({
-                posts: dataw
+            .then(data => this.setState({
+                posts: data
             }));
     }
     render() {
--- front/src/shared/App.js
+++ front/src/shared/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Data from '../Data';
- 
+import {Home, About} from '../pages'
+
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<Data />
+				<Route exact path="/" component={Home}/>
+				<Route exact path="/about" component={About}/>
 			</div>
 		);
 	}	
