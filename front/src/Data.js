3af393a9a86971808922d272c889152a0652fb14
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-31 22:18:14 +0900

    no message

--- front/src/Data.js
+++ front/src/Data.js
@@ -3,19 +3,32 @@ import React,{ Component } from 'react';
 class Data extends Component {
     state = {
         posts : [],
+        params : [
+            syear : '2020',
+            smonth : '1',
+            sday :'1',
+            fyear : '2020'
+            fmonth : '12'
+            fday : '31'
+            prod : '1'
+         ]
     }
     componentWillMount() {
-        fetch('/back/data')
-            .then(response => response.json())
-            .then(data => this.setState({
-                posts: data
-            }));
+        fetch('http://34.64.182.81:8000/calgwp/gwp', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(this.state.params)
+        })
+        .then(response => response.json())
+        .then(data => this.setState({
+            posts: data
+        }));
     }
     render() {
         return (
             <div>
-                hello I am data
-                {this.state.posts.lastname} {this.state.posts.firstname}
+                hello I am data<br>
+                <b>{this.state.posts.gwp}</b>
             </div>
         );
     }
