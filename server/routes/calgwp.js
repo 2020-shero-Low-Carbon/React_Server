e9b39e3f9a93a04b918ad85d9de1b49e91daaf7e
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-31 23:21:55 +0900

    no message

--- server/routes/calgwp.js
+++ server/routes/calgwp.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const {spawn} = require('child_process');
+const path = require('path');
 
 router.post('/gwp', (req, res) => {
 	const dates = req.body;
 	console.log(dates)
+	console.log(proc)
+	const pwd_dir = spawn('pwd');
+	pwd_dir.stdout.on('data', function(data){
+		console.log(data.toString());
+	});
+
 	const py_cal_gwp = spawn('python3', ['./calgwp/S-HERO.py', dates.syear, dates.smonth, dates.sday, dates.fyear, dates.fmonth, dates.fday, dates.prod]);
 	var result = {
 		gwp : 0.
 	};
 	py_cal_gwp.stdout.on('data', function(data){
-		console.log('gwp calculate script running')
-		result.gwp = parseFloat(data.toString())
+		console.log('gwp calculate script running');
+		result.gwp = parseFloat(data.toString());
 	});
 
 	py_cal_gwp.on('close', (code) => {
-		console.log('gwp calculate script finished')
-		res.send()
+		console.log('gwp calculate script finished');
+		res.send();
 	});
 });
 
