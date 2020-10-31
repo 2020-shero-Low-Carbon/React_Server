81e0c724702898471148c9a08d135a16313662e4
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-31 22:28:08 +0900

    no message

--- server/routes/calgwp.js
+++ server/routes/calgwp.js
@@ -6,7 +6,7 @@ router.post('/gwp', (req, res) => {
 	const dates = req.body;
 	const py_cal_gwp = spawn('python3', ['S-HERO.py', dates.syear, dates.smonth, dates.sday, dates.fyear, dates.fmonth, dates.fday, dates.prod]);
 	var result = {
-		gwp = 0.
+		gwp : 0.
 	};
 	py_cal_gwp.stdout.on('data', function(data){
 		console.log('gwp calculate script running')
