c346b7740833972a0c33aab46b7f20f4b0db7035
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-31 23:29:26 +0900

    no message

--- server/routes/calgwp.js
+++ server/routes/calgwp.js
@@ -5,27 +5,29 @@ const path = require('path');
 
 router.post('/gwp', (req, res) => {
 	const dates = req.body;
-	console.log(dates)
+	var my_dir;
+	console.log(dates);
 	const pwd_dir = spawn('pwd');
 	pwd_dir.stdout.on('data', function(data){
-		console.log(data.toString());
+		my_dir = data.toString();
 	});
 	pwd_dir.on('close', (code) =>{
 		console.log('pwd finished');
-	})
-	const py_cal_gwp = spawn('python3', ['./server/routes/S-HERO.py', dates.syear, dates.smonth, dates.sday, dates.fyear, dates.fmonth, dates.fday, dates.prod]);
-	var result = {
-		gwp : 0.
-	};
-	py_cal_gwp.stdout.on('data', function(data){
-		console.log('gwp calculate script running');
-		result.gwp = parseFloat(data.toString());
-	});
+		const py_cal_gwp = spawn('python3', [my_dir + '/server/routes/S-HERO.py', dates.syear, dates.smonth, dates.sday, dates.fyear, dates.fmonth, dates.fday, dates.prod]);
+		var result = {
+			gwp : 0.
+		};
+		py_cal_gwp.stdout.on('data', function(data){
+			console.log('gwp calculate script running');
+			result.gwp = parseFloat(data.toString());
+		});
 
-	py_cal_gwp.on('close', (code) => {
-		console.log('gwp calculate script finished');
-		res.send();
-	});
+		py_cal_gwp.on('close', (code) => {
+			console.log('gwp calculate script finished');
+			res.send();
+		});
+	})
+	
 });
 
 module.exports = router;
