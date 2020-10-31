036abaf057e01f3a3f08cd720fcc1f0525f130ec
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-31 17:39:40 +0900

    no message

--- front/src/Data.js
+++ front/src/Data.js
@@ -3,25 +3,19 @@ import React,{ Component } from 'react';
 class Data extends Component {
     state = {
         posts : [],
-        string : ,
     }
     componentWillMount() {
         fetch('http://34.64.182.81:8000/data')
-            .then(response => this.setState({
-                string: response
-            })
-            );
-            //.then(response => response.json())
-            //.then(data => this.setState({
-            //    posts: data
-            //})
+            .then(response => response.json())
+            .then(data => this.setState({
+                posts: data
+            }));
     }
-        //        {this.state.posts.lastname} {this.state.posts.firstname}
     render() {
         return (
             <div>
                 hello I am data
-                {this.state.string}
+                {this.state.posts.lastname} {this.state.posts.firstname}
             </div>
         );
     }
