0adbc5de3761fc3f8de0f82d363476023e138246
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-10-31 22:41:18 +0900

    no message

--- server/routes/calgwp.js
+++ server/routes/calgwp.js
@@ -4,7 +4,7 @@ const {spawn} = require('child_process');
 
 router.post('/gwp', (req, res) => {
 	const dates = req.body;
-	const py_cal_gwp = spawn('python3', ['S-HERO.py', dates.syear, dates.smonth, dates.sday, dates.fyear, dates.fmonth, dates.fday, dates.prod]);
+	const py_cal_gwp = spawn('python3', ['./server/calgwp/S-HERO.py', dates.syear, dates.smonth, dates.sday, dates.fyear, dates.fmonth, dates.fday, dates.prod]);
 	var result = {
 		gwp : 0.
 	};
