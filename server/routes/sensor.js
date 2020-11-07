240dc3525b51882a5cc8591e620c22e8514bebb9
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-11-08 05:32:05 +0900

    no message

--- server/routes/sensor.js
+++ server/routes/sensor.js
@@ -4,11 +4,12 @@ const mysql = require('mysql');
 const dbconfig = require('../config/database.js');
 const db_connection = mysql.createConnection(dbconfig);
 
+//http://34.64.182.81:8000/sensor/making?am=10&cm=20&ws=30&ms=20&bn=20&wt=50
 router.get('/making', (req, res) => {
 	var today = new Date();
 	date_str = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
 	db_connection.connect();
-	connection.query('insert into test_bed (made_date, cement, wsand, msand, bone, water) values('+date_str+','+req.query.am+','+req.query.cm+','+req.query.ws+','+req.query.ms+','+req.query.bn+','+req.query.wt+')', (error, rows, fields) => {
+	db_connection.query('insert into test_bed (made_date, cement, wsand, msand, bone, water) values('+date_str+','+req.query.am+','+req.query.cm+','+req.query.ws+','+req.query.ms+','+req.query.bn+','+req.query.wt+')', (error, rows, fields) => {
 		if (error) throw error;
 	});
 	db_connection.end();
