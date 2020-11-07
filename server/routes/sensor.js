e713edf6722a80e21543d81f23a0edfe14dec81b
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-11-08 05:34:55 +0900

    no message

--- server/routes/sensor.js
+++ server/routes/sensor.js
@@ -9,7 +9,7 @@ router.get('/making', (req, res) => {
 	var today = new Date();
 	date_str = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
 	db_connection.connect();
-	query_str = 'insert into test_bed (made_date, cement, wsand, msand, bone, water) values('+date_str+','+req.query.am+','+req.query.cm+','+req.query.ws+','+req.query.ms+','+req.query.bn+','+req.query.wt+')';
+	query_str = 'insert into test_bed (made_date, amount, cement, wsand, msand, bone, water) values('+date_str+','+req.query.am+','+req.query.cm+','+req.query.ws+','+req.query.ms+','+req.query.bn+','+req.query.wt+')';
 	console.log(query_str)
 	db_connection.query(query_str, (error, rows, fields) => {
 		if (error) throw error;
