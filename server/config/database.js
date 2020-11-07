549910b87981d6be243c451f5e205ba5d3329c82
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-11-08 01:12:19 +0900

    no message

---
+++ server/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+	host : '34.64.136.15',
+	user : 'root',
+	password : '0000',
+	database : 'data1'
+}
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/calgwp', require('./routes/calgwp'));
+app.use('/map', require('./routes/map'));
 
 app.get('/data',(req,res)=>{
     const data = {
