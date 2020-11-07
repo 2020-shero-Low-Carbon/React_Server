891803f24f731dd725c061e6cf85c2a3fb7425ee
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-11-08 05:43:13 +0900

    no message

--- server/routes/sensor.js
+++ server/routes/sensor.js
@@ -7,6 +7,7 @@ const db_connection = mysql.createConnection(dbconfig);
 //http://34.64.182.81:8000/sensor/making?am=10&cm=20&ws=30&ms=20&bn=20&wt=50
 router.get('/making', (req, res) => {
 	var today = new Date();
+	today.setHours(today.getHours() + 9);
 	date_str = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
 	db_connection.connect();
 	query_str = 'insert into test_bed (made_date, amount, cement, wsand, msand, bone, water) values('+date_str+','+req.query.am+','+req.query.cm+','+req.query.ws+','+req.query.ms+','+req.query.bn+','+req.query.wt+')';
