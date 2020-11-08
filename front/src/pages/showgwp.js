b0f7a6f53f856eec7257c69a1158b50ca90bcc5e
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-11-09 05:22:12 +0900

    no message

---
+++ front/src/pages/showgwp.js
@@ -0,0 +1,40 @@
+import React,{ Component } from 'react';
+ 
+class ShowGWP extends Component {
+    state = {
+        lastgwp : {},
+        mingwp : 0. ,
+        gwplist : {},
+        params : {
+            mode : '0',
+            syear : '2017',
+            smonth : '1',
+            sday :'1',
+            fyear : '2019',
+            fmonth : '12',
+            fday : '31',
+            prod : '1'
+         }
+    }
+    componentWillMount() {
+        fetch('http://34.64.182.81:8000/calgwp/gwplist', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(this.state.params)
+        })
+        .then(response => response.json())
+        .then(result => this.setState({
+            posts: result
+        }));
+    }
+    render() {
+        return (
+            <div>
+                <b>{this.state.params.syear}-{this.state.params.smonth}-{this.state.params.sday} ~ {this.state.params.fyear}-{this.state.params.fmonth}-{this.state.params.fday} , 25-24-150</b><br/>
+                <b>{this.state.posts['2017-01-01']}</b>
+            </div>
+        );
+    }
+}
+
+export default ShowGWP;
--- front/src/shared/App.js
+++ front/src/shared/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Data from '../Data';
 import Data2 from '../Data2';
-import Home from '../pages/Home'
-import About from '../pages/About'
-import Testbed from '../pages/Testbed'
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Testbed from '../pages/Testbed';
+import ShowGWP from '../pages/showgwp';
 
 class App extends Component {
 	render() {
@@ -16,6 +17,7 @@ class App extends Component {
 							<Link to='/'><li>Home</li></Link>
 							<Link to='/about'><li>About Us</li></Link>
 							<Link to='/testbed'><li>Testbed</li></Link>
+							<Link to='/showgwp'><li></li></Link>
 						</ul>
 					</div>
 					<div className='Contents-wrapper'>
@@ -23,6 +25,7 @@ class App extends Component {
 							<Route exact path="/" component={Home}/>
 							<Route exact path="/about" component={About}/>
 							<Route exact path="/testbed" component={Testbed}/>
+							<Route exact path="/showgwp" component={ShowGWP}/>
 							<Route exact path="/data" component={Data}/>
 							<Route exact path="/data2" component={Data2}/>
 						</Switch>
