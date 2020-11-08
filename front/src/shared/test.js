095da9d82844a4091b0048950478b2d086d00bbe
shingeunho <69303637+shingeunho@users.noreply.github.com>
2020-11-09 06:39:33 +0900

    Create test.js

---
+++ front/src/shared/test.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Data from '../Data';
+import Data2 from '../Data2';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Testbed from '../pages/Testbed';
+import ShowGWP from '../pages/showgwp';
+
+import idx from '../html/index.html';
+
+class test extends Component {
+	render() {
+		return (idx);
+	}	
+}
+
+export default test;
