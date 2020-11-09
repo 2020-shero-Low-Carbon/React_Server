b01448c83c893feb93d98bc25559ae1e61af10ba
lss0815 <67047396+lss0815@users.noreply.github.com>
2020-11-09 11:05:40 +0900

    no message

--- front/src/pages/showgwp.js
+++ front/src/pages/showgwp.js
@@ -45,7 +45,7 @@ class ShowGWP extends Component {
 	}
     render() {
         const title_style = {
-            fontsize : '20px'
+            fontsize : '50px'
         }
         return (
             <div>
